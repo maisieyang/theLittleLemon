b02cc07d5285d008d87a083a74c730e9144da80b
yangxiyue <yangxiyuebupt@gmail.com>
2023-06-12 13:35:07 +0800

    feat: menu

---
+++ src/routes/Menu.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+
+
+function Menu() {
+    return (
+        <>
+            <Header />
+            <h1>Menu</h1>
+            <Footer />
+        </>
+    )
+}
+
+export default Menu;
